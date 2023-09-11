e093788039bb5dc5cc022258cd8aeebe801e1bd4
Isaac Lal <isaaclal124@gmail.com>
2023-09-10 23:22:35 -0400

    Formatted if statements with curly braces

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -14,35 +14,44 @@ export const ACTIONS = {
 const reducer = (state, { type, payload }) => {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
-      if (state.overwrite)
+      if (state.overwrite) {
         return {
           ...state,
           currentOperand: payload.digit,
           overwrite: false,
         };
-      if (payload.digit === '0' && state.currentOperand === '0') return state;
-      if (payload.digit === '.' && state.currentOperand.includes('.'))
+      }
+      if (payload.digit === '0' && state.currentOperand === '0') {
         return state;
+      }
+      if (payload.digit === '.' && state.currentOperand.includes('.')) {
+        return state;
+      }
+
       return {
         ...state,
-        currentOperand: `${state.currentOperand || ''} ${payload.digit}`,
+        currentOperand: `${state.currentOperand || ''}${payload.digit}`,
       };
     case ACTIONS.CHOOSE_OPERATION:
-      if (state.currentOperand == null && state.previousOperand == null)
+      if (state.currentOperand == null && state.previousOperand == null) {
         return state;
+      }
 
-      if (state.currentOperand == null)
+      if (state.currentOperand == null) {
         return {
           ...state,
           operation: payload.operation,
         };
-      if (state.previousOperand == null)
+      }
+
+      if (state.previousOperand == null) {
         return {
           ...state,
           operation: payload.operation,
           previousOperand: state.currentOperand,
           currentOperand: null,
         };
+      }
 
       return {
         ...state,
@@ -53,31 +62,30 @@ const reducer = (state, { type, payload }) => {
     case ACTIONS.CLEAR:
       return {};
     case ACTIONS.DELETE_DIGIT:
-      if (state.overwrite)
+      if (state.overwrite) {
         return {
           ...state,
           overwrite: false,
           currentOperand: null,
         };
-
+      }
       if (state.currentOperand == null) return state;
-      if (state.currentOperand.length === 1)
-        return {
-          ...state,
-          currentOperand: null,
-        };
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null };
+      }
 
       return {
         ...state,
-        currentOperand: state.currentOperand.slice(0, -1), // removes last digit from currentOperand
+        currentOperand: state.currentOperand.slice(0, -1),
       };
     case ACTIONS.EVALUATE:
       if (
         state.operation == null ||
         state.currentOperand == null ||
         state.previousOperand == null
-      )
+      ) {
         return state;
+      }
 
       return {
         ...state,
