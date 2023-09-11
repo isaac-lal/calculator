0716ce35d213a4e69fe9691a6df6d83c80f0f612
Isaac Lal <isaaclal124@gmail.com>
2023-09-10 23:33:55 -0400

    Formatted calculator and changed outline

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import Layout from './components/Layout';
-import './index.css'
+import './index.css';
 
 function App() {
   return (
     <>
+      <h1 className='calculator__intro'>Calculator</h1>
       <Layout />
     </>
   );
